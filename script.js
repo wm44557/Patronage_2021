0816c0f30d4d94887c2de22ff4653fe5581eb29c
Marcin Wrzos <marwrz36@gmail.com>
2021-01-27 22:09:38 +0100

    Update list, symbol click and counter

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const moviesCounterSeenDiv = document.getElementById('moviesCounterSeen');
 const moviesCounterAllDiv = document.getElementById('moviesCounterAll');
 const moviesListContainer = document.getElementById('moviesListContainer');
 
-const moviesCounter = moviesData.filter(item => item.seen === 'T');
+let moviesCounter = moviesData.filter(item => item.seen === 'T');
 
 moviesCounterAllDiv.textContent = String(moviesData.length);
 moviesCounterSeenDiv.textContent = String(moviesCounter.length);
@@ -13,29 +13,42 @@ moviesCounterSeenDiv.textContent = String(moviesCounter.length);
 let newElement;
 let itemChildListItem;
 
-const appendChild = (tagName, innerHTML, className) => {
+const renderSeen = (datasetId, index) => {
+    moviesData[index].seen = "T"
+    document.querySelector(`li[data-id='${datasetId}'] .seenDiv`).innerHTML = moviesData[index].seen;
+    moviesCounter = moviesData.filter(item => item.seen === 'T');
+    moviesCounterSeenDiv.textContent = String(moviesCounter.length);
+}
+
+
+const appendChild = (tagName, innerHTML, className, listener) => {
     itemChildListItem = document.createElement(tagName)
     itemChildListItem.innerHTML = innerHTML;
     className ? itemChildListItem.className = className : null;
+    listener ? itemChildListItem.addEventListener('click', function (e) {
+        let datasetId = Number(e.target.parentElement.dataset.id)
+        moviesData.forEach((item, index) => item.id === datasetId ? renderSeen(datasetId, index) : null);
+    }) : null;
     newElement.appendChild(itemChildListItem);
+
 }
 
 moviesData.forEach(function (item, index) {
         newElement = document.createElement('li');
         newElement.id = `list_item_${item.id}`;
+        newElement.dataset.id = item.id;
         newElement.className = "movie";
 
         appendChild('h2', item.title, 'boy');
         appendChild('p', item.year, 'boy');
         appendChild('p', item.summary, 'boy');
-        appendChild('p', item.seen, 'boy');
+        appendChild('div', item.seen, 'seenDiv', true);
 
         moviesListContainer.appendChild(newElement);
     }
 );
 
 //
-// <!-- Stworzy nowy element listy „moviesList” dla każdego elementu tablicy „moviesData”. Elementy listy „moviesList” powinny zawierać wszystkie podstawowe informacje o filmie oraz symbol graficzny reprezentujący informację czy film był lub nie był obejrzany.-->
 // <!-- Obsłuży kliknięcie symbolu graficznego:-->
 // <!-- Przełączy wartość atrybutu „seen” w „moviesData” dla-->
 // <!--danego filmu-->
